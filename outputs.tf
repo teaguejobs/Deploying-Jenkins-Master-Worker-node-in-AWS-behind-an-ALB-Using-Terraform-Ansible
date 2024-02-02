720543b0ab6ba279ea0c5a5ee740613516d0ea5a
teaguejobs <teaguejobs@gmail.com>
2024-02-02 11:04:42 +0000

    update

--- outputs.tf
+++ outputs.tf
@@ -26,3 +26,26 @@ output "Jenkins-Worker-Public-IPs" {
 output "LB-DNS-NAME" {
   value = aws_lb.application-lb.dns_name
 }
+
+output "amiId-us-east-1" {
+  value = data.aws_ssm_parameter.linuxAmi.value
+}
+
+output "amiId-us-west-2" {
+  value = data.aws_ssm_parameter.linuxAmiOregon.value
+}
+
+output "Jenkins-Main-Node-Private-IP" {
+  value = aws_instance.jenkins-master.private_ip
+}
+
+output "Jenkins-Worker-Private-IPs" {
+  value = {
+    for instance in aws_instance.jenkins-worker-oregon :
+    instance.id => instance.private_ip
+  }
+}
+
+output "url" {
+  value = aws_route53_record.jenkins.fqdn
+}
